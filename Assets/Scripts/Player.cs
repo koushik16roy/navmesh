1c2fff458571b1b795e8ecf039422c4caa10cef2
Koushik Roy <66838109+koushik16roy@users.noreply.github.com>
2021-01-14 17:55:45 +0530

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Player : MonoBehaviour
+{
+    public Camera cam;   //camera
+    public NavMeshAgent nv;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray r = cam.ScreenPointToRay(Input.mousePosition);  //take input from mouse from which direction we want to aplayer to move 
+            RaycastHit h;          // creating variable
+
+            if(Physics.Raycast(r, out h))    //add some physcis to store ray cast hit we want 
+            {
+                nv.SetDestination(h.point);
+            }
+         }
+    }
+}
